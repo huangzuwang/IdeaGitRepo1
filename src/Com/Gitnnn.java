73668d763fce6fd74e9c259e756547f6020699ea
huangzuwang <88603356+huangzuwang@users.noreply.github.com>
2021-08-08 14:13:37 +0800

    Initial commit

---
+++ src/Com/Gitnnn.java
@@ -0,0 +1,31 @@
+/**
+ * FileName: Gitnnn
+ * Author:   Administrator
+ * Date:     2021/8/8 13:40
+ * Description:
+ * History:
+ * <p>
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2021/8/8
+ * @since 1.0.0
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2021/8/8
+ * @since 1.0.0
+ */
+
+package Com;
+
+public class Gitnnn {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
